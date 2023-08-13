087ebd6cf1c11ab181896bb69e0cd8155630a31d
jandin88 <jandersson.coutinho@gmail.com>
2023-08-13 18:54:58 -0300

    Create Project

---
+++ src/main/java/com/project/web_servicespring_boot/WebServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.web_servicespring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceSpringBootApplication.class, args);
+    }
+
+}
